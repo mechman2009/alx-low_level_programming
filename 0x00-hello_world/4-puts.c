af2f8c14e83ecc5bf5084a9876f23205b2db5ccc
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-08 17:14:46 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - Printing 
- *Return: Always 0 (Success)
- */
+*main - Printing
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
