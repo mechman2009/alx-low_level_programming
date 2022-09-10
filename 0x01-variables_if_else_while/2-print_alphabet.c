3387d234bd6216c6a9ebdc80027f614529f403cc
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-09 21:24:54 -0700

    Insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - lowercase alphabets 
+ * main - lowercase alphabets
  *
  * Return: Always (Success)
  */
 int main(void)
 {
 char c;
-for (c = 'a'; c <= 'z'; c++) putchar(c);
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
 putchar('\n');
 return (0);
 }
