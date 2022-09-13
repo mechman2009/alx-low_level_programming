764a5ef28556f98ca58dd642dff311cce59083a1
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-12 21:52:48 -0700

    Patience, Persistence and perspirant

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - Prints a series of numbers with commas
+ * main - Prints a series of numbers with commas "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  *
  * Return: Always (Success);
  */
 int main(void)
 {
 int c;
-for (c = '48'; c <= '57'; c++)
+for (c =0; c <= 9; c++)
 putchar(c);
-if (c != '57')
+if (c != 9)
 putchar(',');
 putchar(' ');
 putchar('\n');
