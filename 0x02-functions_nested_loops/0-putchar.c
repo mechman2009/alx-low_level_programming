33b315def1ef69526c5321d4864b27b9e1ad9300
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-14 13:25:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include "main"
+#include "main.h"
+#include <string.h>
 /**
- *  main - print putchar
+ *  main - Entry point
+ *
+ *  Description: prints _putchar
+ *
  *  Return: always 0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+char str[] = "_putchar";
+int ch;
+	
+for (ch = 0; ch < 8; ++ch);
+_putchar(str[ch]);
+_putchar('\n');
 
-	return (0);
+return (0);
 }
