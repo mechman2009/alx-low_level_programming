90bfea994b9a36cb3331369040777aaaba517548
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-15 22:03:40 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
-	/*
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
-	printf("Infinite loop evaded! \\o/\n");
+i = 0;
+/*
+* while (i < 10)
+* {
+* putchar(i);
+* }
+*/
+printf("Infinite loop evaded! \\o/\n");
 
-	return (0);
+return (0);
 }
