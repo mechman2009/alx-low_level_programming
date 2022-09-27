811473145220c18e351abb021c3b0fa660b6b4a0
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-26 17:16:42 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+unsigned int i = 0;
 
-	while (i < n)
+while (i < n)
 
-        {
-	        *(s + i) = b;
-	        i++;
-        }
-        return (s);
+{
+*(s + i) = b;
+i++;
+}
+return (s);
 }
