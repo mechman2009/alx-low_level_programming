6846bd9a041e80d221a771ced2c19efa723e11b4
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-26 18:05:13 -0700

    3-strspn update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,25 @@
 #include "main.h"
+
 /**
-* _strspn - prints the consececutive characters of s1 that are in s2.
-* @s: source string
-* @accept: searching string
+* _strspn - gets length of a prefix substring
+* @s: string to check
+* @accept: string to check against
 *
-* Return: new string.
+* Return: number of bytes of s in accept
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
-	
-for (i = 0; s[i]; i++)
-{
-for (j = 0; accept[j]; j++)
-{
-if (s[i] == accep[j])
-break;
-	
-if (!accept [j])
-break;
-}
-return (i);
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accepted[j])
+			break;
+	}
+	return (i);
 }
