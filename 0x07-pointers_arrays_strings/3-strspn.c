3dc4d6ae946c12d1ceacc74ccc8e1be71a6d1c85
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-26 21:55:32 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,19 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int a = 0, b, t = 0;
-
-        while (accept[a])
-	{
-		b = 0;
-
-		while (s[b] !=32)
-	        {
-			if (accepted[a] == s[b])
-		        {
-				t++;
-			}
-
-		        b++
-                }
-		
-		a++;	
-
-      }
-
-	return (t);
+unsigned int a = 0, b, t = 0;
+while (accept[a])
+{
+b = 0;
+while (s[b] !=32)
+{
+if (accepted[a] == s[b])
+{
+t++;
+}
+b++;
+}		
+a++;	
+}
+return (t);
 }
