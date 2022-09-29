5ed9129bfa3ba013d7049b1c72ba8210122d8969
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-28 17:33:39 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,10 +10,10 @@
 
 int _pow-recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	else
-		return(x * _pow_recursion(x, y - 1));
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return(x * _pow_recursion(x, y - 1));
 }
