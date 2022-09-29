3a28108a32c98e105b67ff7e591465418815ed65
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-28 17:51:40 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,20 +5,16 @@
 * to the power of y
 * @x: second parameter
 * @y: second parameter
-* Return: x power y
+* Return: xraised to power y
 */
 
 int _pow-recursion(int x, int y)
 {
 int result = x;
-
 if (y < 0)
 return (-1);
-
 else if (y == 0)
 return (1);
-
 result*= _pow_recursion(x, y - 1);
-
 return(result);
 }
