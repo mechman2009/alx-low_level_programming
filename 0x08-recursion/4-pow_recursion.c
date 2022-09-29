bca1816bce7ff071981084a93ebeb0041fd14008
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-28 17:28:32 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _pow_recursion - function that return the value of x raised to the power of y
+* @x: second parameter
+* @y: second parameter
+* Return: x power y
+*/
+
+int _pow-recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return(x * _pow_recursion(x, y - 1));
+}
