482eebd70fd233b8cd37c7448a80e447efaa04fe
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-30 17:28:55 -0700

    0-whatsmyname.c task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
-* main -> this is a function to print its name
+* main - this is a function to print its name
+*
 * @argc: argc parameter
 * @argv: an array of command listed
+*
 * Return: 0 for success
 */
 int main(int argc _attribute_((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	(void)argc;
 	return (0);
 }
