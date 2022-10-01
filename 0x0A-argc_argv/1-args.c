ae678d0bd68081d06790d2e83d5035278afdd5f6
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-09-30 17:44:16 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - writes a program that prints the number of argumunts passed into it
+*
+* @args: This is the argument count
+* @argv: This is the ragument vector
+*
+* Return: This return to 0
+*
+*/
+int main(int args, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return(0);
+}
