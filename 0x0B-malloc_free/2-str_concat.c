6246b53baec93ba24e29809454edb0c764c966ae
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-10-04 20:02:49 -0700

    2-str_concat.c updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,8 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	else
 	{
-	for (j = 0; s2[j]; j++)													        ;	
+	for (j = 0; s2[j]; j++)
+	;	
 	}
 	k = i + j + 1;
 	s = malloc(k * sizeof(char));
