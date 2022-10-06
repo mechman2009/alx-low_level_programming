17c1c25271986eb21d0e0ebde228f33aa84012ea
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-10-05 20:25:24 -0700

    100-argstostr.c updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,5 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
 /**
@@ -15,73 +13,38 @@
 * Return: a pointer to a new string
 *
 */
-
 char *argstostr(int ac, char **av)
-
 {
-
 	int i;
-
 	int j;
-
 	char *p = NULL;
-
 	int k;
-
 	int ext;
 
-
-
 	k = 0;
-
 	ext = 0;
-
 	if (ac == 0 || av == NULL)
-
 		return (NULL);
-
 	for (i = 0; i < ac; i++)
-
 	{
-
 		for (j = 0; av[i][j] != '\0'; j++)
-
 		{
-
 			ext++;
-
 		}
-
 	}
-
 	p = (char *)malloc(ext + ac + 1 * sizeof(char));
-
 	if (p == NULL)
-
 		return (NULL);
-
 	for (i = 0; i < ac; i++)
-
 	{
-
 		for (j = 0; av[i][j] != '\0'; j++)
-
 		{
-
 			p[k] = av[i][j];
-
 			k++;
-
 		}
-
 		p[k] = '\n';
-
 		k++;
-
 	}
-
 	p[k] = '\0';
-
 	return (p);
-
 }
