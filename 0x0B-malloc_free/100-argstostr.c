d94c6f5e1339dc04f0b71976bd1a894aec5ddc74
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-10-05 20:48:25 -0700

    100-argstostr.c updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,15 +3,10 @@
 #include <stdlib.h>
 
 /**
-*
 * argstostr - concatenates all the arguments of your program
-*
 * @ac: number of arguments
-*
 * @av: arguments
-*
 * Return: a pointer to a new string
-*
 */
 char *argstostr(int ac, char **av)
 {
