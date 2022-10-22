72afeda65d9b192f65c9903ae818063bb929f3af
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-10-21 21:29:14 -0700

    Advanced Task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 *print - prints before main
 *Return: void.
 */
-void print(void)
+void __attribute__((constructor)) bmain()
 {
 		printf("You're beat! and yet, you must allow,");
 		printf("\nI bore my house upon my back!\n");
