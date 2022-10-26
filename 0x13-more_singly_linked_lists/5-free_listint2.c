5d9de48ac9ab8b9361b72b1a53d90dcd590519bf
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-10-25 21:09:44 -0700

    Task 5 updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,25 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *next;
 
-		listint_t *next;
-
-
-
-			if (head == NULL)
-
-						return;
-
-				while (*head != NULL)
-
-						{
-
-									next = (*head)->next;
-
-											free(*head);
-
-													*head = next;
-
-					        }
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
 
 }
