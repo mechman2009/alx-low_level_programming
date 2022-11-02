64a7015188df5bb2595d417718e3387ce7c7fdfb
Uchenna Okonkwo <okonkwouchenna95@gmail.com>
2022-11-01 21:34:33 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,15 @@ int main(int argc, char *argv[])
 	from_name = argv[1], to_name =  argv[2];
 	f_source = open(from_name, O_RDONLY);
 	if (f_source == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name),
+			exit(98);
 	f_dest = open(to_name, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_dest == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_name), exit(99);
 	read_r = read(f_source, buff, 1024);
 	if (read_r == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name),
+			exit(98);
 	while (read_r > 0)
 	{
 		write_r = write(f_dest, buff, read_r);
